a69ff5b8a22838ac3f90efe5c280afb4ac8f2e79
Lola <lola@egherman.com>
2022-04-14 21:21:22 -0500

    allow Managers to view a MailingListMember

--- src/customModelsEnhanceMap/MailingListMember.ts
+++ src/customModelsEnhanceMap/MailingListMember.ts
@@ -3,7 +3,7 @@ import {Authorized} from "type-graphql";
 import {AuthRole} from "../context";
 
 let defaultPerms: {[key: string]: MethodDecorator[]} = {};
-Object.keys(MailingListMemberScalarFieldEnum).forEach((value: string) => defaultPerms[value] = [Authorized(AuthRole.ADMIN)] )
+Object.keys(MailingListMemberScalarFieldEnum).forEach((value: string) => defaultPerms[value] = [Authorized(AuthRole.ADMIN, AuthRole.MANAGER)] )
 
 export const mailinglistmemberEnhanceConfig: ModelConfig<"MailingListMember"> = {
     fields: {
